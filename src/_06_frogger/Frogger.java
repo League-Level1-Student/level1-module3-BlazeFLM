03b84029988924cb30c150ea9a6e544b16bfbb74
student <student@ip-172-31-60-112.us-west-2.compute.internal>
2022-09-09 15:21:55 -0700

    3:21 PM 9/9/22

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,24 +3,49 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
-    
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	static final int WIDTH = 600;
+	static final int HEIGHT = 400;
+	int frogX;
+	int frogY;
 
-    @Override
-    public void setup() {
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    }
+	@Override
+	public void setup() {
 
-    @Override
-    public void draw() {
+	}
 
-    }
-    static public void main(String[] args) {
-        PApplet.main(Frogger.class.getName());
-    }
+	@Override
+	public void draw() {
+		background(12, 91, 210);
+		fill(3, 180, 50);
+		ellipse(300, 200, 100, 100);
+
+	}
+	void ifKeyPressed(){
+	    if(key == CODED){
+	        if(keyCode == UP)
+	        {
+	            frogY -= 3;
+	        }
+	        else if(keyCode == DOWN)
+	        {
+	            frogY += 3;
+	        }
+	        else if(keyCode == RIGHT)
+	        {
+	        	frogX += 3;
+	        }
+	        else if(keyCode == LEFT)
+	        {
+	            frogX -= 3;
+	        }
+	    }
+	}
+	static public void main(String[] args) {
+		PApplet.main(Frogger.class.getName());
+	}
 }
